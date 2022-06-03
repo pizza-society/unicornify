5eed15aae1efda007cc41d061595d3d232ed7935
Syafiq Basri <syafiq@talentx.ai>
2022-06-03 17:59:40 +0800

    Installed bootstrap, fontawesome; Added navbar component

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+    <i class="fa-brands fa-twitter"></i>
     <h1>This is an about page</h1>
   </div>
 </template>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <MainNavbar></MainNavbar>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import MainNavbar from '@/components/navbars/MainNavbar.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    MainNavbar
   },
 });
 </script>
