fbbe80a0d52e0b004cbc99f4c6dd7ce4b4b5bb5d
Syafiq Basri <syafiq@talentx.ai>
2022-06-08 16:45:36 +0800

    Implemented modal component

--- src/views/TestingView.vue
+++ src/views/TestingView.vue
@@ -1,14 +1,58 @@
 <template>
-    <div></div>
+    <div>
+        <button type="button" 
+            class="btn btn-primary"
+            v-on:click="toggleModal()">
+            Toggle modal
+        </button>
+
+        <ModalBase ref="modalComponent">
+            <template #header>
+                <span>
+                    This is a title
+                </span>
+            </template>
+
+            <template #content>
+                <p>Hello here</p>
+            </template>
+
+            <template #footer>
+                <button type="button"
+                    class="btn btn-secondary"
+                    v-on:click="toggleModal()">
+                    Close
+                </button>
+                <button type="button"
+                    class="btn btn-primary"
+                    v-on:click="toggleModal()">
+                    Save changes
+                </button>
+            </template>
+        </ModalBase>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue';
+import ModalBase from '@/components/modals/ModalBase.vue';
 
 export default defineComponent({
     name: 'TestingView',
     setup() {
-        return {}
+        const modalComponent = ref<InstanceType<typeof ModalBase>>()
+
+        const toggleModal = () => {
+            modalComponent.value?.toggleModal()
+        }
+
+        return {
+            modalComponent,
+            toggleModal
+        }
     },
+    components: {
+        ModalBase
+    }
 })
 </script>
