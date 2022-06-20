71889f0a2c7cedd087fd27aa6f57d82e6572f776
Syafiq Basri <syafiq@talentx.ai>
2022-06-20 12:21:25 +0800

    Installed qrcode, added services, configured rouuter and config

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
 
+from app.services.api import v1 as services
+
 api_router = APIRouter()
+
+api_router.include_router(
+    services.router,
+    prefix='/services',
+    tags=['Services']
+)
