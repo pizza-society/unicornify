e69efae42cb80627a3489a29cbfde1f296d6f455
Syafiq Basri <syafiq@talentx.ai>
2022-06-24 00:30:26 +0800

    Added service module and implementation in testing

--- src/store/modules/services.module.ts
+++ src/store/modules/services.module.ts
@@ -1,8 +1,20 @@
-import { createStore } from 'vuex'
+import { APIService } from '@/common/api.service'
+import { defineStore } from 'pinia'
 
-export default createStore({
-    state: {},
+export const useServiceStore = defineStore('service', {
+    state: () => ({}),
     getters: {},
-    actions: {},
-    mutations: {}
+    actions: {
+        generateQR(payload: object) {
+            return new Promise((resolve, reject) => {
+                APIService.post(`services/generate-qr-code`, payload)
+                    .then(({ data }) => {
+                        resolve(data)
+                    })
+                    .catch(err => {
+                        reject(err)
+                    })
+            })
+        }
+    },
 })
