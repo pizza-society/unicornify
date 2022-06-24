4a20b45488cafeba93515bf32c7cc8a960b34985
Syafiq Basri <syafiq@talentx.ai>
2022-06-25 07:46:56 +0800

    Fixed conflict issue

--- src/components/navbars/NavbarMain.vue
+++ src/components/navbars/NavbarMain.vue
@@ -33,6 +33,8 @@
                         to="/qr-code-generator"
                         active-class="active text-primary">
                         QR Generator
+                    </router-link>
+                    <router-link class="nav-link"
                         to="/email-validator"
                         active-class="active text-primary">
                         Email Validator
