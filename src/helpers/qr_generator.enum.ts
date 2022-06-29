93cdfe4469d49c818f6217c76c96db2c5711daf2
Syafiq Basri <syafiq@talentx.ai>
2022-06-29 19:37:23 +0800

    Added ImageChoices & DrawerChoices

---
+++ src/helpers/qr_generator.enum.ts
@@ -0,0 +1,13 @@
+export enum ImageChoices {
+    PNG = 'png',
+    JPEG = 'jpeg'
+}
+
+export enum DrawerChoices {
+    SQUAREM = 1,
+    GAPPEDM = 2,
+    CIRCLEM = 3,
+    ROUNDEDM = 4,
+    VERTICALM = 5,
+    HORIZONTALM = 6
+}
