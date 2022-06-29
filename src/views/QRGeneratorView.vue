a910622e6492483789182864afeda7af3b78276b
Syafiq Basri <syafiq@talentx.ai>
2022-06-29 13:21:23 +0800

    Implemented validation using vuelidate

--- src/views/QRGeneratorView.vue
+++ src/views/QRGeneratorView.vue
@@ -3,29 +3,52 @@
         <div class="d-flex flex-row vh-min justify-content-center align-items-center row">
             <div class="col-sm-12 col-md-4 text-center">
                <div class="mb-3">
-                    <div class="mb-3">
+                    <form class="needs-validation mb-3">
                         <label class="form-label">Enter URL</label>
                         <input class="form-control"
                             placeholder="Enter link to open when scanned"
                             v-model="urlForm.link"
-                            :disabled="isLoading" />
+                            :disabled="isLoading"
+                            @blur="v$.link.$touch"
+                            :class="{
+                                'is-invalid': v$.link.$error && v$.link.$dirty,
+                                'is-valid': !v$.link.$error && v$.link.$dirty
+                            }" />
+                            <div class="invalid-feedback" 
+                                v-for="error of v$.link.$errors"
+                                :key="error.$uid">
+                                {{ error.$message }}
+                            </div>
+                    </form>
+
+                    <div class="d-grid gap-2 d-flex justify-content-center">
+                        <button class="btn btn-primary"
+                            v-on:click="generateQR()"
+                            :disabled="isLoading || v$.$invalid">
+                            <span v-if="!isLoading">
+                                Generate
+                            </span>
+                            <span v-else>
+                                <i class="fa-solid fa-spinner fa-spin"></i>
+                            </span>
+                        </button>
+
+                        <button class="btn btn-primary"
+                            v-on:click="reset()"
+                            :disabled="generatedResult === null"
+                            v-if="generatedResult !== null">
+                            <span>
+                                Reset
+                            </span>
+                        </button>
                     </div>
-                    <button class="btn btn-primary"
-                        v-on:click="generateQR()"
-                        :disabled="isLoading ||
-                            urlForm.link === null ||
-                            urlForm.link === ''">
-                        <span v-if="!isLoading">
-                            Generate
-                        </span>
-                        <span v-else>
-                            <i class="fa-solid fa-spinner fa-spin"></i>
-                        </span>
-                    </button>
+
+                    
                </div>
 
                 <img v-if="generatedResult" 
-                    :src="`data:image/jpg;base64,${generatedResult}`" />
+                    :src="`data:image/jpg;base64,${generatedResult}`"
+                    class="rounded" />
             </div>
         </div>
     </div>
@@ -35,16 +58,40 @@
 import { defineComponent, ref } from 'vue'
 import { useServiceStore } from '@/store';
 
+import useVuelidate from '@vuelidate/core'
+import { helpers, required, url } from '@vuelidate/validators'
+
 export default defineComponent({
     name: 'QRGeneratorView',
     setup() {
-        const isLoading = ref<boolean>(false)
+
+        // Data
         const generatedResult = ref<string | null | unknown>(null)
+
+        // Service
         const serviceSvc = useServiceStore()
+
+        // Form
         const urlForm = ref({
             link: null
         })
+        const validation = {
+            link: { 
+                required: helpers.withMessage(
+                    'Need to have a value',
+                    required
+                ),
+                url: helpers.withMessage(
+                    'Need to have a valid url',
+                    url
+                )}
+        }
+        const v$ = useVuelidate(validation, urlForm)
 
+        // Checker
+        const isLoading = ref<boolean>(false)
+
+        // Generate QR
         const generateQR = () => {
             isLoading.value = true
 
@@ -58,24 +105,20 @@ export default defineComponent({
             })
         }
 
-        const isValidHttpUrl = () => {
-            let url
-
-            try {
-                url = new URL(String(urlForm.value.link))
-            } catch (_) {
-                return false
-            }
-
-            return url.protocol === 'http:' || url.protocol === 'https:'
+        // Reset service
+        const reset = () => {
+            v$.value.$reset()
+            urlForm.value.link = null
+            generatedResult.value = null
         }
 
         return {
+            generatedResult,
+            urlForm,
+            v$,
             isLoading,
             generateQR,
-            urlForm,
-            generatedResult,
-            isValidHttpUrl
+            reset
         }
     },
 })
