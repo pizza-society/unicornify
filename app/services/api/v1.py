fdd1ab66a1554026804be1352cff7312cc54534b
Syafiq Basri <syafiq@talentx.ai>
2022-06-29 18:28:20 +0800

    Added ImageChoices, DrawerChoices; Updated QRCodeModel and generate_qr to accept options

--- app/services/api/v1.py
+++ app/services/api/v1.py
@@ -4,7 +4,18 @@ import io
 import base64
 import qrcode
 
-from app.services.models import QRCodeModel
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.colormasks import SolidFillColorMask
+from qrcode.image.styles.moduledrawers import (
+    SquareModuleDrawer,
+    GappedSquareModuleDrawer,
+    CircleModuleDrawer,
+    RoundedModuleDrawer,
+    VerticalBarsDrawer,
+    HorizontalBarsDrawer
+)
+
+from app.services.models import DrawerChoices, QRCodeModel
 
 router = APIRouter()
 
@@ -18,14 +29,37 @@ def generate_qr(data: QRCodeModel):
         border=4,
     )
 
+    # Get drawer module
+    if data.drawer == DrawerChoices.GAPPEDM:
+        drawer_module = GappedSquareModuleDrawer()
+    elif data.drawer == DrawerChoices.CIRCLEM:
+        drawer_module = CircleModuleDrawer()
+    elif data.drawer == DrawerChoices.ROUNDEDM:
+        drawer_module = RoundedModuleDrawer()
+    elif data.drawer == DrawerChoices.VERTICALM:
+        drawer_module = VerticalBarsDrawer()
+    elif data.drawer == DrawerChoices.HORIZONTALM:
+        drawer_module = HorizontalBarsDrawer()
+    else:
+        drawer_module = SquareModuleDrawer()
+
     # Generate
     qr.add_data(data.link)
     qr.make(fit=True)
     buffer = io.BytesIO()
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(
+        image_factory=StyledPilImage,
+        module_drawer=drawer_module,
+        color_mask=SolidFillColorMask(
+            front_color=data.front if type(data.front) == tuple else data.front.as_rgb_tuple(),
+            back_color=data.back if type(data.back) == tuple else data.back.as_rgb_tuple()
+        )
+    )
 
-    # Saving
-    img.save(buffer, format="png")
+    # Saving as data type
+    img.save(buffer, format=data.type)
+
     # Encode base 64 and decode utf-8
     img_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
     return img_base64
+
--- app/services/models.py
+++ app/services/models.py
@@ -1,7 +1,26 @@
+from enum import Enum
 from pydantic import BaseModel, HttpUrl
+from pydantic.color import Color
+
+class ImageChoices(str, Enum):
+    PNG = 'png'
+    JPEG = 'jpeg'
+
+class DrawerChoices(int, Enum):
+    SQUAREM = 1
+    GAPPEDM = 2
+    CIRCLEM = 3
+    ROUNDEDM = 4
+    VERTICALM = 5
+    HORIZONTALM = 6
 
 class QRCodeResponse():
     result: str
 
 class QRCodeModel(BaseModel):
     link: HttpUrl
+    type: ImageChoices = ImageChoices.PNG
+    drawer: DrawerChoices = DrawerChoices.SQUAREM
+    front: Color = (0, 0, 0)
+    back: Color = (255, 255, 255)
+
