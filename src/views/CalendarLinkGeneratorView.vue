37f7f3f510e7ada77cc5474d0e9cd4b81a104487
Dev-R <rabimhd@icloud.com>
2022-11-10 23:14:21 +0800

    Added Google Calendar computed property/form

--- src/views/CalendarLinkGeneratorView.vue
+++ src/views/CalendarLinkGeneratorView.vue
@@ -12,101 +12,106 @@
                 </div>
                 <div>
                     <div class="mt-0">
-                        <small class="text-left text-secondary mt-3">
-                        DETAILS
-                        </small>
-                        <hr class="mt-1"/>
-                        <div class="row row-cols-2 g-4 gy-4">
-                            <div class="col-7">
-                                <label class="form-label mb-1">Title</label>
-                                <input class="form-control" placeholder="Add Title" v-model="calendarForm.title"/>
-                            </div>
-                            <div class="col-4">
-                                <label class="form-label mb-1">Location</label>
-                                <input class="form-control" placeholder="Add Location" v-model="calendarForm.location"/>
-                            </div>
-                            <div class="col-7">
-                                <label class="form-label mb-1">Timezone</label>
-                                <!-- https://gist.github.com/nodesocket/3919205 -->
-                                <select name="timezone_offset" id="timezone-offset" class="form-select" v-model="calendarForm.timeZone">
-                                    <option value="-12:00">(GMT -12:00) Eniwetok, Kwajalein</option>
-                                    <option value="-11:00">(GMT -11:00) Midway Island, Samoa</option>
-                                    <option value="-10:00">(GMT -10:00) Hawaii</option>
-                                    <option value="-09:50">(GMT -9:30) Taiohae</option>
-                                    <option value="-09:00">(GMT -9:00) Alaska</option>
-                                    <option value="-08:00">(GMT -8:00) Pacific Time (US &amp; Canada)</option>
-                                    <option value="-07:00">(GMT -7:00) Mountain Time (US &amp; Canada)</option>
-                                    <option value="-06:00">(GMT -6:00) Central Time (US &amp; Canada), Mexico City</option>
-                                    <option value="-05:00">(GMT -5:00) Eastern Time (US &amp; Canada), Bogota, Lima</option>
-                                    <option value="-04:50">(GMT -4:30) Caracas</option>
-                                    <option value="-04:00">(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz</option>
-                                    <option value="-03:50">(GMT -3:30) Newfoundland</option>
-                                    <option value="-03:00">(GMT -3:00) Brazil, Buenos Aires, Georgetown</option>
-                                    <option value="-02:00">(GMT -2:00) Mid-Atlantic</option>
-                                    <option value="-01:00">(GMT -1:00) Azores, Cape Verde Islands</option>
-                                    <option value="+00:00">(GMT) Western Europe Time, London, Lisbon, Casablanca</option>
-                                    <option value="+01:00">(GMT +1:00) Brussels, Copenhagen, Madrid, Paris</option>
-                                    <option value="+02:00">(GMT +2:00) Kaliningrad, South Africa</option>
-                                    <option value="+03:00">(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg</option>
-                                    <option value="+03:50">(GMT +3:30) Tehran</option>
-                                    <option value="+04:00">(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi</option>
-                                    <option value="+04:50">(GMT +4:30) Kabul</option>
-                                    <option value="+05:00">(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent</option>
-                                    <option value="+05:50">(GMT +5:30) Bombay, Calcutta, Madras, New Delhi</option>
-                                    <option value="+05:75">(GMT +5:45) Kathmandu, Pokhara</option>
-                                    <option value="+06:00">(GMT +6:00) Almaty, Dhaka, Colombo</option>
-                                    <option value="+06:50">(GMT +6:30) Yangon, Mandalay</option>
-                                    <option value="+07:00">(GMT +7:00) Bangkok, Hanoi, Jakarta</option>
-                                    <option value="+08:00">(GMT +8:00) Beijing, Perth, Singapore, Hong Kong</option>
-                                    <option value="+08:75">(GMT +8:45) Eucla</option>
-                                    <option value="+09:00">(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk</option>
-                                    <option value="+09:50">(GMT +9:30) Adelaide, Darwin</option>
-                                    <option value="+10:00">(GMT +10:00) Eastern Australia, Guam, Vladivostok</option>
-                                    <option value="+10:50">(GMT +10:30) Lord Howe Island</option>
-                                    <option value="+11:00">(GMT +11:00) Magadan, Solomon Islands, New Caledonia</option>
-                                    <option value="+11:50">(GMT +11:30) Norfolk Island</option>
-                                    <option value="+12:00">(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka</option>
-                                    <option value="+12:75">(GMT +12:45) Chatham Islands</option>
-                                    <option value="+13:00">(GMT +13:00) Apia, Nukualofa</option>
-                                    <option value="+14:00">(GMT +14:00) Line Islands, Tokelau</option>
-                                </select>
-                            </div>
-                            <div class="col-4">
-                                <br/>
-                                <div class="mt-3 form-check">
-                                    <input class="form-check-input" type="checkbox" placeholder="All day event" v-model="calendarForm.isAllDayEvent">
-                                    <label class="form-check-label" for="form-check-inpu">All day event</label>
+                        <form @submit.prevent>
+                            <small class="text-left text-secondary mt-3">
+                            DETAILS
+                            </small>
+                            <hr class="mt-1"/>
+                            <div class="row row-cols-2 g-4 gy-4">
+                                <div class="col-7">
+                                    <label class="form-label mb-1">Title</label>
+                                    <input class="form-control" placeholder="Add Title" v-model="calendarForm.title"/>
+                                </div>
+                                <div class="col-4">
+                                    <label class="form-label mb-1">Location</label>
+                                    <input class="form-control" placeholder="Add Location" v-model="calendarForm.location"/>
+                                </div>
+                                <div class="col-7">
+                                    <label class="form-label mb-1">Timezone</label>
+                                    <select name="timezone_offset" id="timezone-offset" class="form-select" v-model="calendarForm.timeZone">
+                                        <option value="-12:00">(GMT -12:00) Eniwetok, Kwajalein</option>
+                                        <option value="-11:00">(GMT -11:00) Midway Island, Samoa</option>
+                                        <option value="-10:00">(GMT -10:00) Hawaii</option>
+                                        <option value="-09:50">(GMT -9:30) Taiohae</option>
+                                        <option value="-09:00">(GMT -9:00) Alaska</option>
+                                        <option value="-08:00">(GMT -8:00) Pacific Time (US &amp; Canada)</option>
+                                        <option value="-07:00">(GMT -7:00) Mountain Time (US &amp; Canada)</option>
+                                        <option value="-06:00">(GMT -6:00) Central Time (US &amp; Canada), Mexico City</option>
+                                        <option value="-05:00">(GMT -5:00) Eastern Time (US &amp; Canada), Bogota, Lima</option>
+                                        <option value="-04:50">(GMT -4:30) Caracas</option>
+                                        <option value="-04:00">(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz</option>
+                                        <option value="-03:50">(GMT -3:30) Newfoundland</option>
+                                        <option value="-03:00">(GMT -3:00) Brazil, Buenos Aires, Georgetown</option>
+                                        <option value="-02:00">(GMT -2:00) Mid-Atlantic</option>
+                                        <option value="-01:00">(GMT -1:00) Azores, Cape Verde Islands</option>
+                                        <option value="+00:00">(GMT) Western Europe Time, London, Lisbon, Casablanca</option>
+                                        <option value="+01:00">(GMT +1:00) Brussels, Copenhagen, Madrid, Paris</option>
+                                        <option value="+02:00">(GMT +2:00) Kaliningrad, South Africa</option>
+                                        <option value="+03:00">(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg</option>
+                                        <option value="+03:50">(GMT +3:30) Tehran</option>
+                                        <option value="+04:00">(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi</option>
+                                        <option value="+04:50">(GMT +4:30) Kabul</option>
+                                        <option value="+05:00">(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent</option>
+                                        <option value="+05:50">(GMT +5:30) Bombay, Calcutta, Madras, New Delhi</option>
+                                        <option value="+05:75">(GMT +5:45) Kathmandu, Pokhara</option>
+                                        <option value="+06:00">(GMT +6:00) Almaty, Dhaka, Colombo</option>
+                                        <option value="+06:50">(GMT +6:30) Yangon, Mandalay</option>
+                                        <option value="+07:00">(GMT +7:00) Bangkok, Hanoi, Jakarta</option>
+                                        <option value="+08:00">(GMT +8:00) Beijing, Perth, Singapore, Hong Kong</option>
+                                        <option value="+08:75">(GMT +8:45) Eucla</option>
+                                        <option value="+09:00">(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk</option>
+                                        <option value="+09:50">(GMT +9:30) Adelaide, Darwin</option>
+                                        <option value="+10:00">(GMT +10:00) Eastern Australia, Guam, Vladivostok</option>
+                                        <option value="+10:50">(GMT +10:30) Lord Howe Island</option>
+                                        <option value="+11:00">(GMT +11:00) Magadan, Solomon Islands, New Caledonia</option>
+                                        <option value="+11:50">(GMT +11:30) Norfolk Island</option>
+                                        <option value="+12:00">(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka</option>
+                                        <option value="+12:75">(GMT +12:45) Chatham Islands</option>
+                                        <option value="+13:00">(GMT +13:00) Apia, Nukualofa</option>
+                                        <option value="+14:00">(GMT +14:00) Line Islands, Tokelau</option>
+                                    </select>
+                                </div>
+                                <div class="col-4">
+                                    <br/>
+                                    <div class="mt-3 form-check">
+                                        <input class="form-check-input" type="checkbox" placeholder="All day event" v-model="calendarForm.isAllDayEvent">
+                                        <label class="form-check-label" for="form-check-inpu">All day event</label>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <label class="form-check-label" for="date-time-picker">From</label>
+                                    <br/>
+                                    <DatePicker 
+                                        name="date-time-picker"
+                                        :mode = "calendarForm.isAllDayEvent ? 'date' : 'dateTime'" 
+                                        color="purple"
+                                        is-dark
+                                        is-expanded
+                                        :max-date='new Date()'
+                                        v-model="calendarForm.startTime" 
+                                        />
+                                </div>
+                                <div class="col-6">
+                                    <label class="form-check-label" for="date-time-picker">To</label>
+                                    <br/>
+                                    <DatePicker 
+                                        name="date-time-picker"
+                                        :mode = "calendarForm.isAllDayEvent ? 'date' : 'dateTime'" 
+                                        color="purple"
+                                        is-dark
+                                        is-expanded
+                                        :min-date='new Date()'
+                                        v-model="calendarForm.endTime" 
+                                        />
+                                </div>
+                                <div class="col-11">
+                                    <div class="mb-3">
+                                        <label for="form-control" class="form-label">Description</label>
+                                        <textarea class="form-control" rows="3" placeholder="Add description"></textarea>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-6">
-                                <label class="form-check-label" for="date-time-picker">From</label>
-                                <br/>
-                                <DatePicker 
-                                    name="date-time-picker"
-                                    mode="dateTime" 
-                                    color="purple"
-                                    is-dark
-                                    is-expanded
-                                    />
-                            </div>
-                            <div class="col-6">
-                                <label class="form-check-label" for="date-time-picker">To</label>
-                                <br/>
-                                <DatePicker 
-                                    name="date-time-picker"
-                                    mode="dateTime" 
-                                    color="purple"
-                                    is-dark
-                                    is-expanded
-                                    />
-                            </div>
-                            <div class="col-11">
-                                <div class="mb-3">
-                                    <label for="form-control" class="form-label">Description</label>
-                                    <textarea class="form-control" rows="3" placeholder="Add description"></textarea>
-                                </div>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                     <div class="mt-2 mb-5">
                         <small class="text-left text-secondary mt-3">
@@ -130,7 +135,7 @@
                                             <path fill="#1565c0" d="M9,42h5v-8H6v5C6,40.657,7.343,42,9,42z"></path>
                                         </svg>
                                     </span>
-                                    <input type="text" class="form-control" placeholder="Google Calendar" readonly>
+                                    <input type="text" class="form-control" placeholder="Google Calendar"  :value="generateGoogleCalendarLink" readonly>
                                 </div>
                             </div>
                             <div class="col-11">
@@ -179,27 +184,48 @@
     </div>
 </template>
 <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import { DatePicker } from 'v-calendar';
-  export default defineComponent({
-    name: 'CalendarLinkGenerator',
-    setup() {
+import { defineComponent, ref } from "vue";
+import { DatePicker } from "v-calendar";
+import { computed } from "@vue/reactivity";
+export default defineComponent({
+	name: "CalendarLinkGenerator",
+	setup() {
+		// Forms
+		const calendarForm = ref({
+			title: "",
+			location: "",
+			startTime: new Date(),
+			endTime: new Date(),
+			timeZone: new Date(),
+			isAllDayEvent: false,
+			fromDateHour: "",
+			toDateHour: "",
+			description: ""
+		});
 
-      // Forms
-      const calendarForm = ref({
-        title: null,
-        location: null,
-        timeZone: null,
-        isAllDayEvent: false,
-        fromDateHour: null,
-        toDateHour: null,
-        description: null,
-      })
-      return {calendarForm}
-    },
-    components: {
-      DatePicker
-    },
-  });
+		// Methods
+		const convertDateTimeToISO = (dateTimeString: string | Date) => {
+			return new Date(new Date(dateTimeString)).toISOString();
+		};
+
+		console.log("calendarForm", calendarForm.value.title);
+        // https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20221110%2F20221111&details=&location=&text=
+        // https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20221110T151500Z%2F20221110T151500Z&details=&location=&text=
+		const generateGoogleCalendarLink = computed(() => {
+			return `https://www.google.com/calendar/render?action=TEMPLATE&text=${
+				calendarForm.value.title
+			}&details=${calendarForm.value.description}&location=${
+				calendarForm.value.location
+			}&dates=${convertDateTimeToISO(
+				calendarForm.value.startTime
+			)}/${convertDateTimeToISO(calendarForm.value.endTime)}`;
+		});
+
+		return { calendarForm, generateGoogleCalendarLink };
+	},
+	components: {
+		DatePicker
+	}
+});
 </script>
 <style lang="scss" scoped></style>
