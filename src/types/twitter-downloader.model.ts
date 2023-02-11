99b1c2a15b583ce88e14110179f5b635228e53ae
Dev-R <rabimhd@icloud.com>
2023-02-11 19:34:41 +0800

    Updated TwitterDownloader model

--- src/types/twitter-downloader.model.ts
+++ src/types/twitter-downloader.model.ts
@@ -1,16 +1,65 @@
-export default interface TwitterDownloaderResponse{
-    result: {
-        tweet_meta_data: {
-            tweet_url: string,
-            uploader_user_id: string,
-            uploader_display_name: string,
-            upload_date: string,
-            duration: string,
-            thumbnail: string,
-            title: string,
-            description: string
-        },
-        tweet_medias: object,
-        error: string
-    }
+/** Interface for TweetMeta */
+export interface TweetMeta {
+    /** URL of the tweet */
+    tweet_url: string;
+
+    /** ID of the user who uploaded the tweet */
+    uploader_user_id: string;
+
+    /** Display name of the user who uploaded the tweet */
+    uploader_display_name: string;
+
+    /** Date the tweet was uploaded */
+    upload_date: string;
+
+    /** Duration of the tweet media */
+    duration: string;
+
+    /** URL of the thumbnail for the tweet media */
+    thumbnail: string;
+
+    /** Title of the tweet */
+    title: string;
+
+    /** Description of the tweet */
+    description: string;
+}
+
+/** Interface for TweetMedia */
+export interface TweetMedia {
+    /** Format of the tweet media */
+    format: string;
+
+    /** Width of the tweet media */
+    width: number;
+
+    /** Height of the tweet media */
+    height: number;
+
+    /** Resolution of the tweet media */
+    resolution: string;
+
+    /** URL of the tweet media */
+    url: string;
+
+    /** Size of the tweet media */
+    size: string;
+}
+
+/** Interface for TwitterDownloaderResult */
+export interface TwitterDownloaderResult {
+    /** Meta data for the tweet */
+    tweet_meta_data: TweetMeta;
+
+    /** Array of media information for the tweet */
+    tweet_medias: TweetMedia[];
+
+    /** Error message, if any */
+    error: string;
+}
+
+/** Interface for TwitterDownloaderResponse */
+export default interface TwitterDownloaderResponse {
+    /** Result of the Twitter downloader operation */
+    result: TwitterDownloaderResult;
 }
