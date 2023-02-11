ff17b94fc6716153ce0ef27fa543787811e34380
Dev-R <rabimhd@icloud.com>
2023-02-11 18:36:07 +0800

    Updated TikTokDownloader model

--- src/types/tiktok-downloader.model.ts
+++ src/types/tiktok-downloader.model.ts
@@ -1,19 +1,4 @@
-export default interface TikTokDownloaderResponse {
-    result: {
-        medias : TikTokMedias,
-        creator?: string,
-        uploader?: string,
-        title?: string,
-        description?: string,
-        duration_string?: string,
-        _type?: string,
-        thumbnail?: string,
-        format?: string,
-        error?: string,
-    }
-}
-
-export interface TikTokMedias {
+export interface TikTokMedia {
     id : {
         format_id: string 
         format_note: string
@@ -25,13 +10,21 @@ export interface TikTokMedias {
 
 
 export interface TikTokMeta {
-    creator?: string,
-    uploader?: string,
-    title?: string,
-    description?: string,
-    duration_string?: string,
-    _type?: string,
-    thumbnail?: string,
-    format?: string,
-    error?: string,
+    creator: string,
+    uploader: string,
+    title: string,
+    description: string,
+    duration_string: string,
+    _type: string,
+    thumbnail: string,
+    format: string,
+    error: string,
+}
+
+export interface TikTokDownloaderResult extends TikTokMeta {
+    medias: TikTokMedia;
+}
+
+export default interface TikTokDownloaderResponse {
+    result: TikTokDownloaderResult;
 }
