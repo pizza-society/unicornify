e8c882ac3c908b05f21174f4bfd1c80a24526c2c
Dev-R <rabimhd@icloud.com>
2023-03-03 23:57:18 +0800

    Converted regex links to a template

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -3,14 +3,14 @@ import { helpers } from '@vuelidate/validators'
  * Tiktok Link Regex
  * https://stackoverflow.com/questions/74077377/regular-expression-to-match-any-tiktok-video-id-and-url
  * Cases:
- *   - https://m.tiktok.com/h5/share/usr/6641141594707361797.html
- *   - https://m.tiktok.com/v/6749869095467945218.html
- *   - https://www.tiktok.com/embed/6567659045795758085
- *   - https://www.tiktok.com/share/user/6567659045795758085
- *   - https://www.tiktok.com/trending?shareId=6744531482393545985
- *   - https://www.tiktok.com/@burntpizza89/video/7067695578729221378?is_copy_url=1&is_from_webapp=v1
- *   - https://www.tiktok.com/@burntpizza89/video/is_copy_url=1&is_from_webapp=v1&item_id=7067695578729221378
- *   - https://vm.tiktok.com/ZMF6rgvXY/
+ *   - https://m.tiktok.com/h5/share/usr/<id>.html
+ *   - https://m.tiktok.com/v/<id>.html
+ *   - https://www.tiktok.com/embed/<id>
+ *   - https://www.tiktok.com/share/user/<id>
+ *   - https://www.tiktok.com/trending?shareId=<id>
+ *   - https://www.tiktok.com/@user/video/<id>?is_copy_url=1&is_from_webapp=v1
+ *   - https://www.tiktok.com/@user/video/is_copy_url=1&is_from_webapp=v1&item_id=<id>
+ *   - https://vm.tiktok.com/<id>/
  */
 export const tikTokLinkRegex = helpers.regex(
     // eslint-disable-next-line no-useless-escape
