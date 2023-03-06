07af9d46ed85be827caac36e82f61930bd760d07
Dev-R <rabimhd@icloud.com>
2023-03-06 23:02:01 +0800

    Made comment one liner

--- src/types/disposable-email-validator.models.ts
+++ src/types/disposable-email-validator.models.ts
@@ -1,35 +1,23 @@
-/**
- * DisposableEmailMeta interface defines the structure of an object that represents metadata for a disposable email.
- */
+/** DisposableEmailMeta interface defines the structure of an object that represents metadata for a disposable email. */
 export interface DisposableEmailMeta {
-  /**
-   * The format of the email, whether it is valid or not.
-   */
-  format: boolean,
-  /**
-   * The domain of the email.
-   */
-  domain: string,
-  /**
-   * Whether the email is disposable or not.
-   */
-  disposable: boolean,
-  /**
-   * Whether the email has a valid DNS or not.
-   */
-  dns: boolean,
-  /**
-   * Whether the email is whitelisted or not.
-   */
+  /** The format of the email, whether it is valid or not. */
+  format: boolean
+
+  /** The domain of the email. */
+  domain: string
+
+  /** Whether the email is disposable or not. */
+  disposable: boolean
+
+  /** Whether the email has a valid DNS or not. */
+  dns: boolean
+
+  /** Whether the email is whitelisted or not. */
   whitelist: boolean
 }
 
-/**
- * DisposableEmailResponse interface defines the structure of an object that represents the response from validating a disposable email.
- */
+/** DisposableEmailResponse interface defines the structure of an object that represents the response from validating a disposable email. */
 export default interface DisposableEmailResponse {
-  /**
-   * The result of the disposable email validation, represented as a DisposableEmailMeta object.
-   */
+  /** The result of the disposable email validation, represented as a DisposableEmailMeta object. */
   result: DisposableEmailMeta
 }
