773393078d32c8c4f7a445e670cd158293205852
Syafiq Basri <altsyafiq@gmail.com>
2023-03-09 00:35:01 +0800

    :sparkles: Added Encoding enum

--- api/app/v1/constants.py
+++ api/app/v1/constants.py
@@ -1,7 +1,15 @@
 """ API related constants """
+from enum import Enum
+
 
 SERVICES_TAG = "Services"
 SERVICES_ENDPOINT = "/services"
+
+class Encoding(str, Enum):
+    """
+    Enum class for encodings
+    """
+    UTF8 = "utf-8"
 
 
 class RegexPatterns:
