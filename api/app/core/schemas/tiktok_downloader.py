3015b34c23f678683e149542d8a9d2eed551e6da
Syafiq Basri <altsyafiq@gmail.com>
2023-04-01 12:15:36 +0800

    :construction: Improved TikTokVideoDownloaderResponse schema

--- api/app/core/schemas/tiktok_downloader.py
+++ api/app/core/schemas/tiktok_downloader.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, constr
+from typing import List
 
-from app.core.schemas.base import BaseResponse
+from app.core.schemas.camel_case import CamelModel
 from app.v1.constants import RegexPatterns
 
 
@@ -10,8 +11,31 @@ class TikTokVideoDownloaderSchema(BaseModel):
     url: constr(regex=RegexPatterns.TIKTOK_URL_REGEX)
 
 
-class TikTokVideoDownloaderResponse(BaseResponse):
+class TikTokVideoMedia(CamelModel):
+    """ TODO: Rabee to improve if needed """
+
+    format_id: str
+    format_note: str
+    vcodec: str
+    url: str
+    resolution: str
+
+
+class TikTokVideoDownloaderResult(CamelModel):
+    """ TODO: Rabee to improve if needed """
+
+    medias: List[TikTokVideoMedia] = []
+    creator: str
+    uploader: str
+    title: str
+    description: str
+    duration_string: str
+    media_type: str
+    thumbnail: str
+    format: str
+
+
+class TikTokVideoDownloaderResponse(CamelModel):
     """ TODO: add docstring """
 
-    pass
-
+    result: TikTokVideoDownloaderResult
