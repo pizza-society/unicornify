575ae204e1d9af0035741a344f05dc33b041e048
Dev-R <rabimhd@icloud.com>
2023-04-01 18:18:23 +0800

    PS-47: Add docstring for tik_downloader schemas

--- api/app/core/schemas/tiktok_downloader.py
+++ api/app/core/schemas/tiktok_downloader.py
@@ -6,13 +6,27 @@ from app.v1.constants import RegexPatterns
 
 
 class TikTokVideoDownloaderSchema(BaseModel):
-    """ TODO: add docstring """
+    """
+    Represents the schema for a TikTok video downloader request.
+
+    Attributes:
+        url (constr): The TikTok video downloader URL.
+    """
 
     url: constr(regex=RegexPatterns.TIKTOK_URL_REGEX)
 
 
 class TikTokVideoMedia(CamelModel):
-    """ TODO: Rabee to improve if needed """
+    """
+    Represents TikTok video media.
+
+    Attributes:
+        format_id (str): The ID of the format.
+        format_note (str): The format note.
+        vcodec (str): The video codec.
+        url (str): The URL of the video media.
+        resolution (str): The resolution of the video media.
+    """
 
     format_id: str
     format_note: str
@@ -22,7 +36,20 @@ class TikTokVideoMedia(CamelModel):
 
 
 class TikTokVideoDownloaderResult(CamelModel):
-    """ TODO: Rabee to improve if needed """
+    """
+    Represents the result of the TikTok video downloader.
+
+    Attributes:
+        medias (List[TikTokVideoMedia]): The list of media for the video.
+        creator (str): The creator of the video.
+        uploader (str): The uploader of the video.
+        title (str): The title of the video.
+        description (str): The description of the video.
+        duration_string (str): The duration of the video.
+        media_type (str): The type of media in the video.
+        thumbnail (str): The URL of the video thumbnail.
+        format (str): The format of the video.
+    """
 
     medias: List[TikTokVideoMedia] = []
     creator: str
@@ -36,6 +63,11 @@ class TikTokVideoDownloaderResult(CamelModel):
 
 
 class TikTokVideoDownloaderResponse(CamelModel):
-    """ TODO: add docstring """
+    """
+    A response model for the TikTok video downloader.
+
+    Attributes:
+        result (TikTokVideoDownloaderResult): The result of the video downloader.
+    """
 
     result: TikTokVideoDownloaderResult
