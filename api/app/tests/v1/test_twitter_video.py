b0c3d93ea4a55a9b17497f77f1860885f4b269aa
Dev-R <rabimhd@icloud.com>
2023-04-03 02:26:47 +0800

    PS-48: Factor out TwitterVideoDownloader tests

--- api/app/tests/v1/test_twitter_video.py
+++ api/app/tests/v1/test_twitter_video.py
@@ -2,6 +2,9 @@ from fastapi import status
 from fastapi.testclient import TestClient
 
 from app.main import app, get_settings
+from app.core.docs import twitter_video_doc
+from app.tests.v1.test_base import do_test_base_method
+from app.core.schemas.errors import ErrorCode
 from app.v1.constants import SERVICES_ENDPOINT
 
 TEST_ACTION = "/download-tweet-video/"
@@ -9,42 +12,21 @@ TEST_URL = f"{ get_settings().API_V1_STR }{ SERVICES_ENDPOINT }{ TEST_ACTION }"
 client = TestClient(app)
 
 
-def test_twitter_video_methods():
+def test_methods():
     """
     Test that the endpoint only accepts allowed HTTP methods.
     """
-
-    # Test GET method
-    response = client.get(TEST_URL)
-    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-
-    # Test PUT method
-    response = client.put(TEST_URL)
-    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-
-    # Test PATCH method
-    response = client.patch(TEST_URL)
-    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-
-    # Test DELETE method
-    response = client.delete(TEST_URL)
-    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-
-    # Test POST method with missing required field
-    request_data = {}
-    response = client.post(TEST_URL, json=request_data)
-    expected_response_data = {
-        "detail": [
-            {
-                "loc": ["body", "url"],
-                "msg": "field required",
-                "type": "value_error.missing",
-            }
-        ]
+    
+    expected_response_post_data = {
+        "errorCode": ErrorCode.VALIDATION,
+        "errors": {
+            "msg": [
+                "field required"
+            ]
+        }
     }
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    assert response.json() == expected_response_data
 
+    do_test_base_method(client, TEST_URL, expected_response_post_data)
 
 def test_twitter_video_endpoint_with_valid_input():
     """
@@ -54,84 +36,27 @@ def test_twitter_video_endpoint_with_valid_input():
     request_data = {
         "url": "https://twitter.com/Xbox/status/1640020986501165058"
     }
-    expected_output = {
-        "result": {
-            "tweetMetaData": {
-                "tweetUrl": "https://twitter.com/Xbox/status/1640020986501165058",
-                "uploaderUserId": "Xbox",
-                "uploaderDisplayName": "Xbox",
-                "uploadDate": "26-03-2023",
-                "duration": "0:00:10",
-                "thumbnail": "https://pbs.twimg.com/media/FsKGYJPWIAAkQ2i.jpg?name=orig",
-                "title": "Xbox - https://t.co/cXPXo68Ieq",
-                "description": "https://t.co/cXPXo68Ieq",
-            },
-            "tweetMedias": [
-                {
-                    "format": "http-432 - 320x320",
-                    "width": 320,
-                    "height": 320,
-                    "resolution": "320 x 320",
-                    "url": "https://video.twimg.com/amplify_video/1640020879806550018/vid/320x320/CuQ0RG3hOAr4neg9.mp4?tag=16",
-                    "size": "N/A",
-                },
-                {
-                    "format": "http-832 - 540x540",
-                    "width": 540,
-                    "height": 540,
-                    "resolution": "540 x 540",
-                    "url": "https://video.twimg.com/amplify_video/1640020879806550018/vid/540x540/UzdhD_F2tgOdTHY4.mp4?tag=16",
-                    "size": "N/A",
-                },
-                {
-                    "format": "http-1280 - 720x720",
-                    "width": 720,
-                    "height": 720,
-                    "resolution": "720 x 720",
-                    "url": "https://video.twimg.com/amplify_video/1640020879806550018/vid/720x720/XnPHB9v3rX8Ch99-.mp4?tag=16",
-                    "size": "N/A",
-                },
-                {
-                    "format": "http-8768 - 1080x1080",
-                    "width": 1080,
-                    "height": 1080,
-                    "resolution": "1080 x 1080",
-                    "url": "https://video.twimg.com/amplify_video/1640020879806550018/vid/1080x1080/LwPechuE0z3_L8Ut.mp4?tag=16",
-                    "size": "N/A",
-                },
-            ],
-        }
-    }
 
+    expected = twitter_video_doc.RESPONSES[status.HTTP_200_OK]["content"]["application/json"]["example"]
     response = client.post(TEST_URL, json=request_data)
-    response_data = response.json()
 
     assert response.status_code == status.HTTP_200_OK
-    assert response_data == expected_output
+    assert response.json() == expected
 
 
 def test_twitter_video_endpoint_with_invalid_input():
     """
     Test the Twitter video endpoint with invalid input.
     """
+
     request_data = {
         "url": "https://twitter.com/Test123/statux/123445"
     }
-    expected_output = {
-        "detail": [
-            {
-                "loc": ["body", "url"],
-			    "msg": "string does not match regex \"^https?:\\/\\/(?:mobile.)?twitter\\.com\\/(?:#!\\/)?(\\w+)\\/status(?:es)?\\/(\\d+)(?:\\/.*)?(\\?(.+))?$\"",
-                "type": "value_error.str.regex",
-                "ctx": {
-                    "pattern": "^https?:\\/\\/(?:mobile.)?twitter\\.com\\/(?:#!\\/)?(\\w+)\\/status(?:es)?\\/(\\d+)(?:\\/.*)?(\\?(.+))?$"
-                },
-            }
-        ]
-    }
+    expected = twitter_video_doc.RESPONSES[status.HTTP_422_UNPROCESSABLE_ENTITY]["content"]["application/json"]["example"]
+
 
     response = client.post(TEST_URL, json=request_data)
     response_data = response.json()
 
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    assert response_data == expected_output
+    assert response_data == expected
