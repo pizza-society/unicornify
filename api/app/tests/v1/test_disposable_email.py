6d529662de06aa8cf9ced8709534eac3935f8a25
Dev-R <rabimhd@icloud.com>
2023-04-03 04:45:15 +0800

    PS-49 Factor out DisposableEmail tests

--- api/app/tests/v1/test_disposable_email.py
+++ api/app/tests/v1/test_disposable_email.py
@@ -2,6 +2,9 @@ from fastapi import status
 from fastapi.testclient import TestClient
 
 from app.main import app, get_settings
+from app.tests.v1.test_base import do_test_base_method
+from app.core.docs import disposable_email_doc
+from app.core.schemas.errors import ErrorCode
 from app.v1.constants import SERVICES_ENDPOINT
 
 TEST_ACTION = "/validate-disposable-email/"
@@ -9,91 +12,48 @@ TEST_URL = f"{ get_settings().API_V1_STR }{ SERVICES_ENDPOINT }{ TEST_ACTION }"
 client = TestClient(app)
 
 
-def test_disposable_email_methods():
+def test_methods():
     """
     Test that the endpoint only accepts allowed HTTP methods.
-
-    This test function makes requests to the endpoint with various HTTP methods to verify that
-    only the expected methods are allowed. It also tests the behavior when a required field is missing
-    in the request payload.
     """
-
-    # Test GET method
-    response = client.get(TEST_URL)
-    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-
-    # Test PUT method
-    response = client.put(TEST_URL)
-    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-
-    # Test PATCH method
-    response = client.patch(TEST_URL)
-    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-
-    # Test DELETE method
-    response = client.delete(TEST_URL)
-    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-
-    # Test POST method with missing required field
-    request_data = {}
-    response = client.post(TEST_URL, json=request_data)
-    expected_response_data = {
-        "detail": [
-            {
-                "loc": ["body", "email"],
-                "msg": "field required",
-                "type": "value_error.missing",
-            }
-        ]
+    expected_response_post_data = {
+        "errorCode": ErrorCode.VALIDATION,
+        "errors": {
+            "msg": [
+                "field required"
+            ]
+        }
     }
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    assert response.json() == expected_response_data
+
+    do_test_base_method(client, TEST_URL, expected_response_post_data)
 
 
 def test_disposable_email_endpoint_with_valid_input():
     """
     Test the Disposable Email endpoint with valid input.
-
-    This function tests the Disposable Email endpoint by sending a request with valid email parameters.
     """
 
-    request_data = {"email": "hello@github.com"}
-    expected_output = {
-        "result": {
-            "format": True,
-            "domain": "github.com",
-            "disposable": False,
-            "dns": True,
-        }
+    request_data = {
+        "email": "hello@github.com"
     }
-
+    expected = disposable_email_doc.RESPONSES[status.HTTP_200_OK]["content"]["application/json"]["example"]
     response = client.post(TEST_URL, json=request_data)
 
-    response_data = response.json()
-
     assert response.status_code == status.HTTP_200_OK
-    assert response_data == expected_output
+    assert response.json() == expected
 
 
 def test_disposable_email_endpoint_with_invalid_input():
     """
     Test the Disposable Email endpoint with invalid input.
-
-    This function tests the Disposable Email endpoint by sending a request with an invalid email parameters.
     """
-    request_data = {"email": "www.google.com"}
-    expected_output = {
-        "detail": [
-            {
-                "loc": ["body", "email"],
-                "msg": "value is not a valid email address",
-                "type": "value_error.email"
-            }
-        ]
+
+    request_data = {
+        "email": "www.google.com"
     }
 
+    expected = disposable_email_doc.RESPONSES[status.HTTP_422_UNPROCESSABLE_ENTITY]["content"]["application/json"]["example"]
     response = client.post(TEST_URL, json=request_data)
-    response_data = response.json()
 
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    assert response_data == expected_output
+    assert response.json() == expected
