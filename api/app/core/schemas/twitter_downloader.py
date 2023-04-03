e0aea6c6644065a882fe58d4431a51f881677b9a
Dev-R <rabimhd@icloud.com>
2023-04-03 17:23:21 +0800

    PS-48: Added indentation to classes docstring

--- api/app/core/schemas/twitter_downloader.py
+++ api/app/core/schemas/twitter_downloader.py
@@ -9,6 +9,7 @@ from app.core.schemas.camel_case import CamelModel
 class TwitterVideoDownloaderSchema(BaseModel):
     """
     Represents the schema for a Twitter video downloader request.
+
     Attributes:
         url (constr): The Twitter video downloader URL.
     """
@@ -16,9 +17,10 @@ class TwitterVideoDownloaderSchema(BaseModel):
     url: constr(regex=RegexPatterns.TWITTER_URL_REGEX)
 
 
-class tweetMedias(CamelModel):
+class TweetMedias(CamelModel):
     """
     Represents a media object in a tweet.
+
     Attributes:
         format (str): The format of the media.
         width (int): The width of the media.
@@ -36,9 +38,10 @@ class tweetMedias(CamelModel):
     size: str
 
 
-class tweetMetaData(CamelModel):
+class TweetMetaData(CamelModel):
     """
     Represents metadata for a tweet.
+
     Attributes:
         tweet_url (str): The URL of the tweet.
         uploader_user_id (str): The user ID of the uploader of the tweet.
@@ -63,18 +66,20 @@ class tweetMetaData(CamelModel):
 class TwitterVideoDownloaderResult(CamelModel):
     """
     Represents the result of a Twitter video downloader.
+
     Attributes:
-        tweet_meta_data (tweetMetaData): The metadata of the tweet.
-        tweet_medias (List[tweetMedias]): The list of media objects in the tweet.
+        tweet_meta_data (TweetMetaData): The metadata of the tweet.
+        tweet_medias (List[TweetMedias]): The list of media objects in the tweet.
     """
 
-    tweet_meta_data: tweetMetaData
-    tweet_medias: List[tweetMedias] = []
+    tweet_meta_data: TweetMetaData
+    tweet_medias: List[TweetMedias] = []
 
 
 class TwitterVideoDownloaderResponse(CamelModel):
     """
     A response model for the Twitter video downloader.
+    
     Attributes:
         result (TwitterVideoDownloaderResult): The result of the video downloader.
     """
