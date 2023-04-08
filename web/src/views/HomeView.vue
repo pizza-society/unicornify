da224fb9ceea90f6d32a95e7ff40a2226cf7e107
Syafiq Basri <altsyafiq@gmail.com>
2023-04-09 02:41:05 +0800

    PS-36: Reduced image size

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -3,7 +3,7 @@
 		<div class="d-flex flex-column vh-min justify-content-center align-items-center">
 			<img
 				src="@/assets/images/outer-space.svg"
-				style="height: 30rem; width:auto" />
+				style="height: 20rem; width:auto" />
 			<p>
 				Phew phew
 			</p>
