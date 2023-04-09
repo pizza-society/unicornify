be4487a72e3dd8f6998c8b7da590e970c4442b59
Syafiq Basri <altsyafiq@gmail.com>
2023-04-09 10:38:02 +0800

    PS-36: Moved template.compilerOptions.isCustomElement into vite.config from vue.config to fix unresolved component issue

--- web/vue.config.js
+++ web/vue.config.js
@@ -8,10 +8,7 @@ module.exports = defineConfig({
 			.use('vue-loader')
 			.tap(options => ({
 				...options,
-				compilerOptions: {
-					// treat any tag that starts with lottie- as custom elements
-					isCustomElement: tag => tag.startsWith('lottie-')
-				}
+				compilerOptions: { }
 			}))
 	}
 })
