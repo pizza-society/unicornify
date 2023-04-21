15f02a37cb8f8bd08b7f61a03e1ea26080911bc1
Dev-R <rabimhd@icloud.com>
2023-04-21 18:24:51 +0800

    PS-61: Refactored code

--- web/src/views/TikTokDownloaderView.vue
+++ web/src/views/TikTokDownloaderView.vue
@@ -1,137 +1,141 @@
 <template>
 	<div class="container">
-		<div
-			class="d-flex flex-column justify-content-center align-items-center">
-			<lottie-player
-				v-if="!tikTokMediaData"
-				src="https://assets7.lottiefiles.com/packages/lf20_5h2kp8uz.json"
-				background="transparent"
-				speed="0.5"
-				style="width: 300px; height: 300px;"
-				loop
-				autoplay />
+		<div class="d-flex flex-column justify-content-center align-items-center">
+			<div class="col-sm-12 col-md-4 text-center">
+				<div class="header-container">
+					<lottie-player
+						v-if="!tikTokMediaData"
+						src="https://assets7.lottiefiles.com/packages/lf20_5h2kp8uz.json"
+						background="transparent"
+						class="mx-auto"
+						speed="1"
+						style="width: 15rem; height: 15rem;"
+						autoplay />
+				</div>
 
-			<div class="col-lg-8 text-center">
-				<figure 
-					v-if="!tikTokMediaData && !formError"
-					class="text-center">
-					<blockquote class="blockquote">
-						<h3 class="mb-4">
+				<div class="text-center">
+					<div 
+						v-if="!tikTokMediaData && !formError"
+						class="text-center">
+						<h3 class="mb-0">
 							TikTok Downloader
 						</h3>
-					</blockquote>
 
-					<figcaption class="blockquote-footer">
-						Download TikTok Videos in seconds...
-					</figcaption>
-				</figure>
+						<p class="mb-4 text-secondary">
+							<small>
+								Download TikTok Videos in seconds...
+							</small>
+						</p>
+					</div>
 
-				<ErrorAlert v-if="formError">
-					The TikTok link you supplied does not include any video 
-					or that there is a problem downloading the video.
-				</ErrorAlert>
+					<ErrorAlert v-if="formError">
+						The TikTok link you supplied does not include any video 
+						or that there is a problem downloading the video.
+					</ErrorAlert>
 		
-				<Transition>
-					<div v-if="tikTokMediaData && tikTokMetaData && !isLoading && !formError">
-						<div class="card text-bg-dark mb-3 mt-5">
-							<div class="row g-0">
-								<div class="col-md-4">
-									<img
-										class="img-fluid rounded-start"
-										:src="tikTokMetaData.thumbnail" />
-								</div>
-
-								<div class="col-md-8">
-									<div class="card-header">
-										<h5 class="card-title">
-											<i class="fa-solid fa-user"></i>
-											{{ tikTokMetaData.creator }}
-										</h5>
+					<Transition>
+						<div v-if="tikTokMediaData && tikTokMetaData && !isLoading && !formError">
+							<div class="card text-bg-dark mb-3 mt-5">
+								<div class="row g-0">
+									<div class="col-md-4">
+										<img
+											class="img-fluid rounded-start"
+											:src="tikTokMetaData.thumbnail" />
 									</div>
 
-									<div class="card-body">
-										<p class="card-text">
-											{{ tikTokMetaData.description }}
-										</p>
-									</div>
+									<div class="col-md-8">
+										<div class="card-header">
+											<h5 class="card-title">
+												<i class="fa-solid fa-user"></i>
+												{{ tikTokMetaData.creator }}
+											</h5>
+										</div>
 
-									<div class="card-body">
-										<ul>
-											<li
-												v-for="(item, index) in tikTokMediaData"
-												:key="index"   
-												type="button"     
-												placeholder="Enter link (Eg: https://www.tiktok.com/@unicornify/video/0123456789)"                                                      
-												class="list-group-item thumbnail m-2 p-2 bg-primary rounded"
-												@click="downloadVideo(item.url)">
-												<i class="fa-solid fa-circle-arrow-down"></i>
+										<div class="card-body">
+											<p class="card-text">
+												{{ tikTokMetaData.description }}
+											</p>
+										</div>
 
-												<span>
-													Download {{ item.formatNote }} [{{ Number(index) + 1 }}]
-												</span>
-											</li>
-										</ul>
-									</div>
+										<div class="card-body">
+											<ul>
+												<li
+													v-for="(item, index) in tikTokMediaData"
+													:key="index"   
+													type="button"     
+													class="list-group-item thumbnail m-2 p-2 bg-primary rounded"
+													@click="downloadVideo(item.url)">
+													<i class="fa-solid fa-circle-arrow-down"></i>
+
+													<span>
+														Download {{ item.formatNote }} [{{ Number(index) + 1 }}]
+													</span>
+												</li>
+											</ul>
+										</div>
                                     
-									<div class="card-footer">
-										<i class="fa-solid fa-video"></i>
-										{{ convertToTime(tikTokMetaData.durationString) }}
+										<div class="card-footer">
+											<i class="fa-solid fa-video"></i>
+											{{ convertToTime(tikTokMetaData.durationString) }}
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</Transition>
-			</div>
-			<!-- Form -->
-			<div class="col-lg-5 text-center">                  
-				<form
-					class="needs-validation"
-					@submit.prevent>
-					<div class="row rounded-top">
-						<div class="input-group">
-							<div class="input-group mb-3">
-								<TheInput 
-									v-model="downloadForm.url"
-									type="text"
-									placeholder="Enter Video Link..."
-									:disabled="isLoading"
-									:class="{
-										'is-invalid': v$.url.$error && v$.url.$dirty,
-										'is-valid': !v$.url.$error && v$.url.$dirty
-									}"
-									@blur="v$.url.$touch" />
-									
-								<div
-									v-for="error of v$.url.$errors"
-									:key="error.$uid"
-									class="invalid-feedback">
-									{{ error.$message }}
+					</Transition>
+
+					<div class="mb-3">    
+						<!-- Form -->
+						<form
+							class="needs-validation"
+							@submit.prevent>
+							<div class="row rounded-top">
+								<label class="form-label text-center">
+									Enter URL
+								</label>
+
+								<div class="input-group mb-3">
+									<TheInput 
+										v-model="downloadForm.url"
+										type="text"
+										placeholder="Enter link (Eg: https://tiktok.com/@user/video/1234)"
+										:disabled="isLoading"
+										:class="{
+											'is-invalid': v$.url.$error && v$.url.$dirty,
+											'is-valid': !v$.url.$error && v$.url.$dirty
+										}"
+										@blur="v$.url.$touch" />
+								
+									<div
+										v-for="error of v$.url.$errors"
+										:key="error.$uid"
+										class="invalid-feedback">
+										{{ error.$message }}
+									</div>
 								</div>
 							</div>
-						</div>
+						</form>
+
+						<button
+							class="btn btn-primary"
+							type="button"
+							:disabled="isLoading || v$.$invalid"
+							@click="getTikTokMedia()">
+							<span v-if="!isLoading">
+								Download
+							</span>
+
+							<span v-else>
+								<span
+									class="spinner-grow spinner-grow-sm"
+									role="status"
+									aria-hidden="true">
+								</span>
+								Loading...
+							</span>
+						</button>
 					</div>
-				</form>
-
-				<button
-					id="button-addon2"
-					class="btn btn-primary"
-					type="button"
-					:disabled="isLoading || v$.$invalid"
-					@click="getTikTokMedia()">
-					<span v-if="!isLoading">
-						Download
-					</span>
-
-					<span v-else>
-						<span
-							class="spinner-grow spinner-grow-sm"
-							role="status"
-							aria-hidden="true">
-						</span>
-						Loading...
-					</span>
-				</button>
+				</div>
 			</div>
 		</div>
 	</div>
